8c9142af6c773f7d9304cc93c0126e0a062e2251
Coder-developer-1 <134141234+Coder-developer-1@users.noreply.github.com>
2023-10-23 07:33:38 +0000

    swiper carousel code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay} from 'swiper/modules';
+import 'swiper/css';
+import A from "./assets/aa (1).jpg";
+import B from "./assets/bb.jpg";
+import C from "./assets/cc.jpg";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <div><h1>Namaste World</h1></div>
+
+      <div className='carousel-container'>
+        <h3> bappa Morya </h3>
+      <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 900,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      className="mySwiper"
+    >
+      <SwiperSlide> <img src={A}/> </SwiperSlide>
+      <SwiperSlide> <img src={B}/> </SwiperSlide>
+      <SwiperSlide> <img src={C}/> </SwiperSlide>
+    
+    </Swiper>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+// https://codesandbox.io/s/pdrc53?file=/src/App.jsx:671-745
